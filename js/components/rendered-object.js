393e7e9c9d769941528dd20bdf72d74e0e0cec32
Chimène Blokesch <chimene.blokesch2002@gmail.com>
2023-05-11 17:00:48 +0200

    Frustum culling

--- js/components/rendered-object.js
+++ js/components/rendered-object.js
@@ -6,17 +6,17 @@ AFRAME.registerComponent('rendered-object', {
 
     init: function () {
         // Initializes the type of the rendering.
-        var rendererData = this.data.renderer.getDOMAttribute("renderer");
+        // var rendererData = this.data.renderer.getDOMAttribute("renderer");
         this.rendererComponent = this.data.renderer.components.renderer;
 
         // Initialize the type of the rendering.
         this.rendererComponent.chooseType(this.el, true);
 
-        if (ALGO2LOD[rendererData.renderingAlgo] != null) {
-            // No need to update the type of the rendering. Therefore
-            // this component isn't needed anymore.
-            this.el.removeAttribute("rendered-object");
-        }
+        // if (ALGO2LOD[rendererData.renderingAlgo] != null) {
+        //     // No need to update the type of the rendering. Therefore
+        //     // this component isn't needed anymore.
+        //     this.el.removeAttribute("rendered-object");
+        // }
     },
 
     tick: function (time, timeDelta) {
--- js/components/renderer.js
+++ js/components/renderer.js
@@ -23,6 +23,8 @@ AFRAME.registerComponent('renderer', {
         // Copy the LODs. When a LOD is loaded,
         // the key of the LOD will be removed.
         this.loadedEntities = Object.values(LOD);
+        this.frustum = new THREE.Frustum();
+        this.matrix = new THREE.Matrix4();
     },
 
     /* Creates a a-entity element with instanced mesh component
@@ -63,6 +65,19 @@ AFRAME.registerComponent('renderer', {
      * When init is set to true, it will always change. The changes are based
      * on the used rendering algorithm. */
     chooseType: function (el, init = false) {
+        // https://stackoverflow.com/questions/49902680/aframe-entity-is-seen
+        // Only render the object when it is visible by the camera.
+        var cam = this.el.sceneEl.camera;
+
+        if (cam) {
+            this.frustum.setFromProjectionMatrix(this.matrix.multiplyMatrices(cam.projectionMatrix,
+                cam.matrixWorldInverse));
+            if (!this.frustum.containsPoint(el.object3D.position)) {
+                // The point is not visible, don't render it.
+                return this.setModel(el, null);
+            }
+        }
+
         var thLowSprite;
 
         switch (this.data.renderingAlgo) {
@@ -88,16 +103,22 @@ AFRAME.registerComponent('renderer', {
 
     /* Sets the model corresponding to the LOD to the object. */
     setModel: function (el, lod) {
-        if (el.getAttribute("lod") == lod) {
+        if (lod == null) {
+            // Nothing to render, so remove the old model.
+            el.removeAttribute("instanced-mesh-member");
+            return;
+        }
+
+        if (el.getAttribute("instanced-mesh-member") == "mesh:#lod" + lod) {
             // No change.
             return;
         }
 
-        // Assure the model is loaded.
-        this.loadModel(lod);
-
         // Remove the old model.
         el.removeAttribute("instanced-mesh-member");
+
+        // Assure the model is loaded.
+        this.loadModel(lod);
 
         // Add the new model.
         el.setAttribute("instanced-mesh-member", "mesh:#lod" + lod);
