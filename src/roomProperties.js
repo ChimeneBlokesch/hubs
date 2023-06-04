d270175c68cf8ab89d0b0d59aa9cd78b04d67268
Chimène Blokesch <chimene.blokesch2002@gmail.com>
2023-06-04 13:26:57 +0200

    Changes to parameters hubs room and test room

--- src/roomProperties.js
+++ src/roomProperties.js
@@ -33,13 +33,13 @@ var roomVlooienburg = {
         "paths":
         {
             "mid": {
-                "minX": 21.50,
-                "maxX": 23,
+                "minX": 22,
+                "maxX": 25,
                 "minZ": -67.50,
                 "maxZ": -13.50,
                 "amountNPCs": 100,
-                "cellSizeX": 2,
-                "cellSizeZ": 2,
+                "cellSizeX": 1,
+                "cellSizeZ": 1.5,
                 "speedNPC": 0.2,
                 "rotationNPC": 180,
                 "idRenderer": "renderer1",
@@ -47,13 +47,13 @@ var roomVlooienburg = {
                 "colorPlane": "red"
             },
             "left": {
-                "minX": 21,
-                "maxX": 21.50,
+                "minX": 20.50,
+                "maxX": 22,
                 "minZ": -67.50,
                 "maxZ": -13.50,
                 "amountNPCs": 50,
                 "cellSizeX": 1,
-                "cellSizeZ": 1.5,
+                "cellSizeZ": 0.6,
                 "speedNPC": 0,
                 "rotationNPC": 90,
                 "idRenderer": "renderer1",
@@ -61,10 +61,79 @@ var roomVlooienburg = {
                 "colorPlane": "blue"
             },
             "right": {
-                "minX": 24.50,
-                "maxX": 25,
+                "minX": 25,
+                "maxX": 26.50,
                 "minZ": -67.50,
                 "maxZ": -13.50,
+                "amountNPCs": 50,
+                "cellSizeX": 1,
+                "cellSizeZ": 0.6,
+                "speedNPC": 0,
+                "rotationNPC": -90,
+                "idRenderer": "renderer1",
+                "walkReversed": true,
+                "colorPlane": "green"
+            }
+        }
+    }
+};
+
+var roomTest = {
+    "Test Ruimte Scriptie Mass Simulation Vlooienburg 1625": {
+        "renderers":
+        {
+            "renderer1": {
+                "renderingFiles": {
+                    "sprite": null,
+                    "low": low,
+                    "medium": medium,
+                    "high": high
+                },
+                "renderingAlgo": "model_low",
+                "distanceThresholds": [
+                    15,
+                    30,
+                    500,
+                    1000
+                ],
+                "capacity": 1000
+            }
+        },
+        "paths":
+        {
+            "mid": {
+                "minX": -4,
+                "maxX": 3,
+                "minZ": -500,
+                "maxZ": 0,
+                "amountNPCs": 100,
+                "cellSizeX": 2,
+                "cellSizeZ": 1.5,
+                "speedNPC": 0.2,
+                "rotationNPC": 180,
+                "idRenderer": "renderer1",
+                "walkReversed": true,
+                "colorPlane": "red"
+            },
+            "left": {
+                "minX": -10,
+                "maxX": -5,
+                "minZ": -500,
+                "maxZ": -2,
+                "amountNPCs": 50,
+                "cellSizeX": 1,
+                "cellSizeZ": 2,
+                "speedNPC": 0,
+                "rotationNPC": 90,
+                "idRenderer": "renderer1",
+                "walkReversed": true,
+                "colorPlane": "blue"
+            },
+            "right": {
+                "minX": 4,
+                "maxX": 9,
+                "minZ": -500,
+                "maxZ": -2,
                 "amountNPCs": 50,
                 "cellSizeX": 1,
                 "cellSizeZ": 1.5,
@@ -78,5 +147,4 @@ var roomVlooienburg = {
     }
 };
 
-
-export var rooms = { ...roomVlooienburg };
+export var rooms = { ...roomVlooienburg, ...roomTest };
